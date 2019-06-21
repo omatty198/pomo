ce1a3d22f64c8ac7aae34ce537bd222f969f77fa
omatty <9mm.cool@gmail.com>
2019-06-22 01:04:47 +0900

    Initial Commit

---
+++ NewPomo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  NewPomo
+//
+//  Created by omatty198 on 2019/06/22.
+//  Copyright © 2019 omatty198. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
